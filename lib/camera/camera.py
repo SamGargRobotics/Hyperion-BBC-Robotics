ca5d0bdde5bedb5735d4c8484fc3e9056e6b4014
Timmy Macarbey <111961513+timmy69420@users.noreply.github.com>
2025-01-30 16:33:37 +1000

    Update camera.py

--- lib/camera/camera.py
+++ lib/camera/camera.py
@@ -40,11 +40,11 @@ def YellowFind():
         #print("X Dist: "+str(XZero-blob.cx()))
         XDIST = XZero-blob.cx()
         if(XDIST < 0):
-            XDIST*=-1
+            #XDIST*=-1
         #print("Y Dist: "+str(YZero-blob.cy()))
         YDIST = YZero-blob.cy()
         if(YDIST < 0):
-            YDIST*=-1
+            #YDIST*=-1
         HDIST = sqrt((XDIST*XDIST)+(YDIST*YDIST))
         return [XDIST,YDIST,HDIST]
         #img.draw_string(0,0,"Blue Len: "+str(XDIST),13,color=(0,0,255))
@@ -67,11 +67,11 @@ def BlueFind():
         #print("X Dist: "+str(XZero-blob.cx()))
         XDIST = XZero-blob.cx()
         if(XDIST < 0):
-            XDIST*=-1
+            #XDIST*=-1
         #print("Y Dist: "+str(YZero-blob.cy()))
         YDIST = YZero-blob.cy()
         if(YDIST < 0):
-            YDIST*=-1
+            #YDIST*=-1
         HDIST = sqrt((XDIST*XDIST)+(YDIST*YDIST))
         return [XDIST,YDIST,HDIST]
         #img.draw_string(0,0,"Blue Len: "+str(XDIST),13,color=(0,0,255))
