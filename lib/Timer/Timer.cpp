231e7b56ad4075f59f3ee4aa159b02fe17ca051d
Sam Garg <132874866+SamGargRobotics@users.noreply.github.com>
2025-06-17 11:26:26 +1000

    Updated Timer Library

--- lib/Timer/Timer.cpp
+++ lib/Timer/Timer.cpp
@@ -1,14 +1,25 @@
+/*! 
+ * @file Timer.cpp
+ */
 #include "Timer.h"
 
+/*! 
+ * @brief Initialize the timer based on the users constraints.
+ */
 Timer::Timer(unsigned long duration) {
     timerDuration = duration;
 }
 
+/*! 
+ * @brief Function to reset the timer based on the current micros.
+ */
 void Timer::resetTime() {
     lastUpdate = micros();
 }
 
-
+/*! 
+ * @brief Check if the set time has passed since the last reset time.
+ */
 bool Timer::timeHasPassed() {
     if (micros() - lastUpdate > timerDuration) {
         resetTime();
@@ -18,6 +29,10 @@ bool Timer::timeHasPassed() {
     return false;
 }
 
+/*!
+ * @brief Check if the set time has passed since the last reset time, but do not
+ *        reset the timer.
+ */
 bool Timer::timeHasPassedNoUpdate() {
     return micros() - lastUpdate > timerDuration;
 }
--- lib/Timer/Timer.h
+++ lib/Timer/Timer.h
@@ -1,9 +1,21 @@
+/*!
+ * @file Timer.h
+ * 
+ * @mainpage This is a library for a inbuilt timer inside the code
+ * 
+ * @date 17/06/25
+ * 
+ * @author J.Huang (Brisbane Boys' College)
+ */
 #ifndef TIMER_H
 #define TIMER_H
 
 #include <Arduino.h>
 
-// Timer class. Check if a certain time has elapsed
+/*!
+ * @brief Class that stores states and functions for interacting with the
+ *        in-built Arduino timer.
+ */
 class Timer {
 public:
     Timer(unsigned long duration);
